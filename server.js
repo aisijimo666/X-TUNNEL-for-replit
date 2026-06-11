fbed403fd7dfc6defc5e6feca617a7d8ea6faee3
aisijimo666 <163801764+aisijimo666@users.noreply.github.com>
2026-06-11 16:21:31 +0800

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ const http = require('http');
 const fs = require('fs');
 const path = require('path');
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
+
 const HOST = '0.0.0.0';
 
 const MIME_TYPES = {
